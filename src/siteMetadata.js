530633a128e4fbc6e2d6f9f99306a96b0a007df2
williambaibai <williambaibai@gmail.com>
2019-12-30 19:50:42 +0800

    Use create-react-blog

---
+++ src/siteMetadata.js
@@ -0,0 +1,11 @@
+export default {
+  // The blog's title as it appears in the layout header, and in the document
+  // <title> tag.
+  title: "William Bai's Homepage",
+
+  author: "William Bai",
+  description: "A personal blog and portfolio website for William, a senior at Cornell University.",
+
+  // The number of posts to a page on the site index.
+  indexPageSize: 10,
+}
