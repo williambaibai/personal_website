015ebec3410c6081fd3bf548542fd60df42f11b5
williambaibai <williambaibai@gmail.com>
2019-12-30 22:25:27 +0800

    Add navigation

---
+++ README.md
@@ -0,0 +1,5 @@
+# William Bai's Personal Website
+This is the personal blog and portfolio website for William, a senior at Cornell University.
+
+## Citations
+This website is bootstrapped with [create-react-blog](https://github.com/jamesknelson/create-react-blog).
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import styles from './Bio.module.css'
-import { getGravatarURL } from '../utils/getGravatarURL'
+import portrait from '../static/portrait.jpg'
 
 function Bio(props) {
-  let photoURL = getGravatarURL({
-    email: "test1@example.com",
-    size: 56,
-  })
-
   return (
     <div className={`
       ${styles.Bio}
       ${props.className || ''}
     `}>
-      <img src={photoURL} alt="Me" />
+      <img src={portrait} alt="Me" />
       <p>
-        Create a blog with a single command, by{' '}
-        <a href="https://twitter.com/james_k_nelson/">James K Nelson</a>.
-        <br />
-        Themed after Gatsby's blog starter and Dan Abramov's{' '}
-        <a href="https://overreacted.io/">overreacted.io</a>.<br />
-        Based on{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          create-react-app
-        </a>
-        , <a href="https://mdxjs.com/">MDX</a>, and{' '}
-        <a href="https://frontarm.com/navi/">Navi</a>.
+        Welcome! I am a senior studying computer science at Cornell.
+        <br/>
+        This is my personal blog and portfolio site. 
+        <br/>
+        If you find my content interesting, please leave a comment :)
       </p>
     </div>
   )
