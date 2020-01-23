c73e5cfd06afc97df49085b0d01cc30ad0898439
williambaibai <williambaibai@gmail.com>
2020-01-23 16:07:28 -0500

    blog1

---
+++ src/routes/posts/2020-01-21-intro/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Why I Created this Blog`,
+  tags: ['miscellaneous', 'introspection'],
+  spoiler: "Welcome to my blog! Click to learn more about this site.",
+  getContent: () => import('./document.mdx'),
+}
